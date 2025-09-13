90999498911421d6ec4a56465c950f758937846c
Christian Kaiser <kaiser_christian06@outlook.de>
2025-09-13 13:33:37 +0200

    alles von main auf eine logic-klasse gezogen. so bleibt main sauber. wie funktioniert das mit GUI? aktuell wird daran gearbeitet.

---
+++ logic.py
@@ -0,0 +1,36 @@
+import os #todo kann noch später in die gui klasse eingebaut werden
+
+from Battle import *
+from Enemy import *
+from Map import *
+from Player import *
+from GUI import *
+
+class logic:
+    def __init__(self):
+        return
+
+    def start(self):
+
+        print(f'''***********************************************************
+        HERZLICH WILLKOMMEN BEI MEINEN ERSTEN INTERAKTIVEN DUNGEON-CRAWLER!!!
+        Ich hoffe dass er euch gefällt!
+        ***********************************************************''')
+
+        Spielername = (str(input("Bitte gebe nun deinen Namen ein: ")))
+        ork = Gegner(200, 50, 20, 'Ork')
+        Kampf = Battle()
+        Menue = GUI()
+        User = Player(Spielername, 100, 10, 20, 2, 4)
+        Spielfeld = Map(30, " P", " G", " _", " W", " I")
+        Menue.Abstandshalter()
+
+        while True:
+
+            Spielfeld.setPlayer(User.positionX,User.positionY)
+            Spielfeld.zeigeKarte()
+            Menue.hauptmenue()
+            Spielfeld.movePlayer(User)
+            os.system('cls')  # Windows
+
+        #Kampf.Encounter(True, User, ork)
--- main.py
+++ main.py
@@ -1,34 +1,9 @@
-import os
+from logic import *
 
-from Battle import *
-from Enemy import *
-from Map import *
-from Player import *
-from GUI import *
+Spiel = logic()
 
-print(f'''***********************************************************
-HERZLICH WILLKOMMEN BEI MEINEN ERSTEN INTERAKTIVEN DUNGEON-CRAWLER!!!
-Ich hoffe dass er euch gefällt!
-***********************************************************''')
-
-Spielername = (str(input("Bitte gebe nun deinen Namen ein: ")))
-ork = Gegner(200, 50, 20, 'Ork')
-Kampf = Battle()
-Menue = GUI()
-User = Player(Spielername, 100, 10, 20, 2, 4)
-Spielfeld = Map(30, " P", " G", " _", " W", " I")
-Menue.Abstandshalter()
-
-while 1 == 1:
-
-    Spielfeld.setPlayer(User.positionX,User.positionY)
-    Spielfeld.zeigeKarte()
-    Menue.hauptmenue()
-    Spielfeld.movePlayer(User)
-    os.system('cls')  # Windows
-#   break
+Spiel.start()
 
 
-Kampf.Encounter(True, User, ork)
 
 
