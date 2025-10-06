aaf8a7ace442a91213e3a3f81c796df558aa45c0
DarkCode34 <60271159+DarkCode34@users.noreply.github.com>
2025-10-06 14:23:27 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 from logic import *
-#import ctypes
-
-# Fensterhandle holen
-#hwnd = ctypes.windll.kernel32.GetConsoleWindow()
-
-# 3 = SW_MAXIMIZE
-#ctypes.windll.user32.ShowWindow(hwnd, 3)
 
 Spiel = logic()
 
